b9fd3dc629d507cddbd93fd235d6044df5c6e369
Christopher Bazley <119876938+chrisbazley@users.noreply.github.com>
2026-08-10 15:00:41 +0200

    Suppress clang-analyzer-unix.Stream) locally
    Closing stdin or stdout would be stupid

--- main.c
+++ main.c
@@ -53,6 +53,8 @@ enum {
                      the compression algorithm */
 };
 
+/* NOLINTBEGIN(clang-analyzer-unix.Stream) */
+// Closing stdin or stdout would be stupid
 static bool process_file(_Optional const char * const input_file,
                          _Optional const char * const output_file,
                          _Optional const char *song_name,
@@ -173,6 +175,7 @@ static bool process_file(_Optional const char * const input_file,
 
   return success;
 }
+/* NOLINTEND(clang-analyzer-unix.Stream) */
 
 static int syntax_msg(FILE * const f, const char * const path)
 {
