24a728556aec81a327979a59cb276a1d0cf558cd
Christopher Bazley <119876938+chrisbazley@users.noreply.github.com>
2026-08-28 15:21:56 +0100

    Add optional handling for file operations

--- tests/Decompress.c
+++ tests/Decompress.c
@@ -15,6 +15,12 @@
 #include "fortify.h"
 #endif
 
+#ifdef USE_OPTIONAL
+#include "Optional.h"
+#else
+#define _Optional
+#endif
+
 enum {
   BufferSize = 256,
   HistoryLog2 = 9
@@ -40,25 +46,25 @@ int main(int argc, char *argv[])
   if (argc != 3)
     return EXIT_FAILURE;
 
-  FILE *const input = fopen(argv[1], "rb");
+  _Optional FILE *const input = fopen(argv[1], "rb");
   if (input == NULL)
     return EXIT_FAILURE;
 
-  FILE *const output = fopen(argv[2], "wb");
+  _Optional FILE *const output = fopen(argv[2], "wb");
   if (output == NULL) {
-    fclose(input);
+    fclose(&*input);
     return EXIT_FAILURE;
   }
 
   Reader reader;
-  bool success = reader_gkey_init(&reader, HistoryLog2, input);
+  bool success = reader_gkey_init(&reader, HistoryLog2, &*input);
 
   if (success) {
     unsigned char buffer[BufferSize];
     size_t count;
 
     while ((count = reader_fread(buffer, 1, sizeof(buffer), &reader)) > 0) {
-      if (fwrite(buffer, 1, count, output) != count) {
+      if (fwrite(buffer, 1, count, &*output) != count) {
         success = false;
         break;
       }
@@ -70,9 +76,9 @@ int main(int argc, char *argv[])
     reader_destroy(&reader);
   }
 
-  if (fclose(output) != 0)
+  if (fclose(&*output) != 0)
     success = false;
-  if (fclose(input) != 0)
+  if (fclose(&*input) != 0)
     success = false;
 
   return success ? EXIT_SUCCESS : EXIT_FAILURE;
